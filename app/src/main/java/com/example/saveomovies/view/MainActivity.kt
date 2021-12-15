65120f11a65764239001008758e5d8f4f9960b30
sahilk01 <khan.lesnar786@gmail.com>
2021-12-16 05:28:11 +0530

    implemented a toolbar with navigation ui

--- app/src/main/java/com/example/saveomovies/view/MainActivity.kt
+++ app/src/main/java/com/example/saveomovies/view/MainActivity.kt
@@ -2,19 +2,32 @@ package com.example.saveomovies.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.saveomovies.R
+import com.example.saveomovies.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupToolbar()
+
+    }
+
+    private fun setupToolbar() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+//        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
