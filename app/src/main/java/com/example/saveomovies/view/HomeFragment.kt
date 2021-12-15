270af389e6ceb0ddf3e8a91ae8bffc397be90312
sahilk01 <khan.lesnar786@gmail.com>
2021-12-16 04:49:42 +0530

    implemented coordinator layout for neseted scrolling

--- app/src/main/java/com/example/saveomovies/view/HomeFragment.kt
+++ app/src/main/java/com/example/saveomovies/view/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -27,7 +28,6 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private val homeViewModel: HomeViewModel by viewModels()
-    private lateinit var popularMoviesAdapter: PopularMoviesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
